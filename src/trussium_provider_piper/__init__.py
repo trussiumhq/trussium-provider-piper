789365540dba88fe2d3d67d5d00cbb86d7fcac81
sartim <sarrtim@gmail.com>
2026-08-26 23:22:23 +0300

    feat: add Piper speech adapter

---
+++ src/trussium_provider_piper/__init__.py
@@ -0,0 +1,5 @@
+"""Piper provider plugin for Trussium."""
+
+from trussium_provider_piper.speech import PiperSpeechCapability, PiperProviderError
+
+__all__ = ["PiperProviderError", "PiperSpeechCapability"]
